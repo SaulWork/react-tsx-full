7f19c5333be8b08471676213b28d3b11aee368a0
SaulWork <msaraujo@knowmadmood.com>
2026-03-12 18:02:08 +0000

    added title

--- ejemplos-saul/02-lab-propiedades/src/App.tsx
+++ ejemplos-saul/02-lab-propiedades/src/App.tsx
@@ -4,6 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div>
+      <h1>14.4. Lab Propiedades</h1>
       <Sugus sabor="limón" color="#FDE23A" />
       <Sugus sabor="naranja" color="#F28E40" />
       <Sugus sabor="piña" color="#227BBE" />
