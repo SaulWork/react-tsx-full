8108247b1e19ccae93c162cda7b62a4dd00ab68d
SaulWork <msaraujo@knowmadmood.com>
2026-03-16 18:01:34 +0000

    added children type

--- ejemplos-saul/08-lab-props-children/src/components/Acordeon/index.tsx
+++ ejemplos-saul/08-lab-props-children/src/components/Acordeon/index.tsx
@@ -1,28 +1,30 @@
-import { useState, useEffect, JSX } from "react";
+import { useState, useEffect, ReactNode, ReactElement } from "react";
 import "./Acordeon.css";
 
 type Props = {
-  children: JSX.Element;
+  children: ReactElement<React.HTMLAttributes<HTMLUListElement>, "ul">; // ReactNode;
   titulo: string;
   cerrado?: boolean;
 };
 
 const Acordeon: React.FC<Props> = ({ children, titulo, cerrado = true }) => {
-
-  const [estaCerrado, setEstaCerrado] = useState<boolean | null>(null);
-
+  const [estaCerrado, setEstaCerrado] = useState<boolean>(cerrado);
 
   useEffect(() => {
     setEstaCerrado(cerrado);
   }, [cerrado]);
 
-
   return (
     <div className="acordeon">
-      <div className="acordeon-heading" onClick={() => setEstaCerrado(!estaCerrado)}>
+      <div
+        className="acordeon-heading"
+        onClick={() => setEstaCerrado(!estaCerrado)}
+      >
         <h3>{titulo}</h3>
       </div>
-      <div className={"acordeon-content " + (estaCerrado ? "cerrado" : "abierto")}>
+      <div
+        className={"acordeon-content " + (estaCerrado ? "cerrado" : "abierto")}
+      >
         {children}
       </div>
     </div>
