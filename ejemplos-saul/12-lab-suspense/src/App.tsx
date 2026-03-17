85739bbfbca13a827aa53f923bad58d197bef52b
SaulWork <msaraujo@knowmadmood.com>
2026-03-17 15:56:59 +0000

    added 12-lab-suspense

---
+++ ejemplos-saul/12-lab-suspense/src/App.tsx
@@ -0,0 +1,24 @@
+import "./App.css";
+import { Suspense, lazy, useState } from 'react';
+import Inicio from "./components/Inicio";
+const Admin = lazy(() => import('./components/Admin'));
+
+
+function App() {
+  const [esAdmin, setEsAdmin] = useState(false);
+
+  return (
+    <div>
+    <button type="button" onClick={() => setEsAdmin(!esAdmin)}>Toggle admin</button>
+            { esAdmin ?
+                <Suspense fallback={<p>Loading...</p>}>
+                  <Admin />
+                </Suspense>
+            :
+
+              <Inicio />
+            }
+      </div>
+  );
+}
+export default App;
